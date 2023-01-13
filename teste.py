0b2a4d12cd1de1a48c0be65b6be97f1364f10997
viniciusvianavieira <vieiravianavinicius@gmail.com>
2023-01-13 08:10:42 -0300

    ok

--- teste.py
+++ teste.py
@@ -19,32 +19,36 @@ username = getpass.getuser()
 driver = webdriver.Chrome(options=options)
 driver.get(web)
 
-time.sleep(5)
-inspect = '/html/body/div[1]/div/section[2]/div[5]/div[2]/div/div/div[3]/div/div'
-element = driver.find_element('xpath',f'{inspect}')
+time.sleep(10)
+inspect = '/html/body/div[1]/div/section[2]/div[5]/div[2]/div/div/div[3]/div'
+element = driver.find_element('xpath', f'{inspect}')
 
 html_element = element.get_attribute('outerHTML')
 
-print(html_element)
-
 soup = BeautifulSoup(html_element, 'html.parser')
 
-header = soup.find("div",{"class":"live-events-league__header live-events-league__header--clickable"})
-print(header.text)
+header = soup.find("div", {
+                   "class": "live-events-league__header live-events-league__header--clickable"})
 
-body = soup.find_all("div",{"class":"live-events-event-row__container live-event live-events-event-row__container--row"})
+body = soup.find_all("div", {
+                     "class": "live-events-event-row__container live-event live-events-event-row__container--row"})
 
-for respostas in body: 
-    print("********************************************************************************************************")
-    print(respostas)
-    print("********************************************************************************************************")
+# with open("./text.html", "w") as f:
+#     f.write(str(body))
+
+for data in body:
+    participantes = data.find_all(
+        "span", {"class": "live-event__participants__participant-name"})
+    print(participantes[0].text)
+    print(participantes[1].text)
 
 
-
-
-
-
-
+# cont = 0
+# for respostas in body:
+#     print("*"*30)
+#     print(respostas.find_all("div", {
+#         "class": "live-events-league__header live-events-league__header--clickable"}))
+#     print("*"*30)
 
 # participants = body.find_all("span",{"class":"live-event__participants__participant-name"})[0]
 # odds = body.find_all("span",{"class":"selections__selection__odd"})
@@ -55,4 +59,3 @@ for respostas in body:
 # result = body.find_all("span",{"class":"live-event__scores__score__text"})
 # print(result[0].text)
 # print(result[1].text)
-
